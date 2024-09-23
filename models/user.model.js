ad8e782db80bd0fac34ed0cdf3815b11ffc1e96b
Ahmed79723 <ashrafahmed797@gmail.com>
2024-09-23 17:28:00 +0300

    5th commit

--- models/user.model.js
+++ models/user.model.js
@@ -1,5 +1,5 @@
 import mongoose, { Schema, model } from "mongoose";
-import bcrypt from "bcrypt";
+import bcryptjs from "bcryptjs";
 
 const userSchema = new Schema(
   {
@@ -64,10 +64,10 @@ const userSchema = new Schema(
 );
 
 userSchema.pre("save", function () {
-  this.password = bcrypt.hashSync(this.password, 8);
+  this.password = bcryptjs.hashSync(this.password, 8);
 });
 userSchema.pre("findOneAndUpdate", function () {
   if (this._update.password)
-    this._update.password = bcrypt.hashSync(this._update.password, 8);
+    this._update.password = bcryptjs.hashSync(this._update.password, 8);
 });
 export const User = model("User", userSchema);

7a1fe725887d23c891a03d95d9e804088c1502b1
Ahmed79723 <ashrafahmed797@gmail.com>
2024-09-23 19:10:04 +0300

    12th commit

--- dataBase/dbConnection.js
+++ dataBase/dbConnection.js
@@ -1,7 +1,14 @@
 import mongoose from "mongoose";
 
 export const dbConnection = mongoose
-  .connect("mongodb+srv://Rana:rana014500@e-commerce.j07rt.mongodb.net/E-CommerceDB")
+  .connect(
+    "mongodb+srv://Rana:rana014500@e-commerce.j07rt.mongodb.net/E-CommerceDB",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      serverSelectionTimeoutMS: 5000, // Increase this value as needed
+    }
+  )
   .then(() => {
     console.log("database connected successfully!");
   });
